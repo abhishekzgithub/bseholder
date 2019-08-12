0cf3a591e0dbfa59b2a574c47fe19204c9bb8e70
Kumar <abhishekx.x.kumar@intel.com>
2019-08-12 15:12:09 +0530

    saving the work2

--- PromoterNGroup.py
+++ PromoterNGroup.py
@@ -1,7 +1,24 @@
 import requests
-import bs4
-URL=r'https://www.bseindia.com/corporates/shpPromoterNGroup.aspx?scripcd=532500&qtrid=102.00&QtrName=June%202019'
-r = requests.get(URL)
-soup = bs4.BeautifulSoup(r.content, 'html5lib') 
-print(soup.prettify())
+import pandas as pd
+from lxml import html
 
+def get_table_df():
+    try:
+        URL=r'https://www.bseindia.com/corporates/shpPromoterNGroup.aspx?scripcd=532500&qtrid=102.00&QtrName=June%202019'
+        #URL=r'https://www.bseindia.com/corporates/shpPublicShareholder.aspx?scripcd=532500&qtrid=101.00&QtrName=June%202019'
+        r = requests.get(URL)
+        tree = html.fromstring(r.content)
+        cols=tree.xpath('//*[@class="innertable_header1"]/text()')
+        df=pd.DataFrame(columns=cols)
+        for i in range(0,len(cols)):
+            tr_elements=tree.xpath(f'//tr[3]//td[{i+1}]')
+            if i==0:
+                df[cols[i]]=[(T.text_content()) for T in tr_elements][-7:]
+            #print(tr_elements)
+            else:
+                df[cols[i]]=[(T.text_content()) for T in tr_elements]
+    except ValueError:
+        print(i)
+        print(ValueError)
+    return df.iloc[1:,:]
+get_table_df()
