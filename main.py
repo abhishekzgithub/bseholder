9004a3442202e7aa8ff1387ea2130ab9ce3f977b
Kumar <abhishekx.x.kumar@intel.com>
2019-08-13 15:02:35 +0530

    Note: this is the main.py in working condition

--- main.py
+++ main.py
@@ -2,18 +2,11 @@ import requests
 import pandas as pd
 from lxml import html
 import re
-def get_extra_col_df(cols):
-    for ix,val in enumerate(cols):
-        if ix==5:
-            df[cols[ix]+'-'+cols[-2]]=" "
-            df[cols[ix]+'-'+cols[-1]]=" "
-            continue
-        elif ix>=7:
-            pass
-        else:
-            df[val]=" "
-    return df
-class PromoternGroup(object):
+
+class PromoterNGroup(object):
+    def __init__(self,URL):
+        self.URL=URL
+
     def get_column_name(self,tree):
         cols=tree.find_class('innertable_header1')
         cols=[cols[i].text_content().encode('ascii', 'ignore').decode("utf-8") for i in range(len(cols))]
@@ -29,9 +22,8 @@ class PromoternGroup(object):
         cols=[(cols[i].text_content()).encode('ascii', 'ignore').decode("utf-8") for i in range(len(cols))]
         return cols
     
-    def fit_data(self,tree,cols,df):
+    def fit_data(self,tree,cols):
         datum=self.get_data(tree)
-        print(cols)
         data={}
         j=1
         for i in range(0,len(datum),len(cols)-1):
@@ -41,12 +33,48 @@ class PromoternGroup(object):
         df1.columns=cols[1:]
         return df1
     
-    def col_having_six(self,URL):
-        r = requests.get(URL)
+    def col_having_six(self):
+        r = requests.get(self.URL)
         tree = html.fromstring(r.content)
         cols=self.get_column_name(tree)
         df=pd.DataFrame()
-        df=(self.fit_data(tree,cols,df))
+        df=(self.fit_data(tree,cols))
         df[cols[0]]=self.get_right_column_data(tree)
         return df.replace(r'^\s*$', 0, regex=True)
 
+class PromoterNGroupVariation(PromoterNGroup):
+    def __init__(self,URL):
+        self.URL=URL
+
+    def get_extra_col_df(self,cols):
+        df=pd.DataFrame()
+        for ix,val in enumerate(cols):
+            if ix==5:
+                df[cols[ix]+'->'+cols[-2]]=" "
+                df[cols[ix]+'->'+cols[-1]]=" "
+                continue
+            elif ix>=7:
+                pass
+            else:
+                df[val]=" "
+        return list(df.columns[1:])
+
+    def fit_data(self,tree,cols):
+        datum=self.get_data(tree)
+        data={}
+        j=1
+        for i in range(0,len(datum),7):
+            data[j]=datum[i:i+7]
+            j+=1
+        df1=pd.DataFrame(data).T
+        df1.columns=self.get_extra_col_df(cols)
+        return df1
+if __name__=='__main__':
+    try:
+        URL=''
+        obj1=PromoterNGroup(URL)
+        df=obj1.col_having_six()
+    except ValueError as v:
+        obj1=PromoterNGroupVariation(URL)
+        df=obj1.col_having_six()
+        
